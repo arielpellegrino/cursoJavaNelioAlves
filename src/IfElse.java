699f5ccd5a5310b76da5ddf6e59ec3b825ff42ca
Ariel Pellegrino Iborra <ariel.iborra@sensedia.com>
2023-04-10 21:36:30 -0300

    adicionando aulas

--- src/IfElse.java
+++ src/IfElse.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class IfElse {
+    public static void main(String[] args) {
+        int x = 5;
+        int hora;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Quantas horas?");
+        hora = sc.nextInt();
+
+        if (hora < 12){
+            System.out.println("bom dia");
+        } else {
+            if(hora < 18){
+                System.out.println("Boa tarde");
+            } else {
+                System.out.println("Boa noite");
+            }
+        }
+
+        sc.close();
+    }
 }
