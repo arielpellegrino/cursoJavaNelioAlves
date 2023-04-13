d799a4ad1d096bda3239d871112e13fd010d6609
Ariel Pellegrino Iborra <ariel.iborra@sensedia.com>
2023-04-13 19:49:59 -0300

    laço for

--- src/CondicionalTernaria.java
+++ src/CondicionalTernaria.java
@@ -1,2 +1,18 @@
 public class CondicionalTernaria {
+    public static void main(String[] args) {
+        double preco = 34.5;
+        double desconto;
+
+        if (preco < 20.0) {
+            desconto = preco * 0.1;
+        }
+        else {
+            desconto = preco * 0.05;
+        }
+
+        double descontoTernario = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+        System.out.println(desconto);
+        System.out.println(descontoTernario);
+    }
 }
