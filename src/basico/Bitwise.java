28a36dc32b6784bcc754efb67ac892b760bcb646
Ariel Pellegrino <ariel.pell@gmail.com>
2025-04-11 10:01:31 -0300

    update code

--- src/basico/Bitwise.java
+++ src/basico/Bitwise.java
@@ -1,10 +1,19 @@
 package basico;
 import java.util.Scanner;
 
-public class Program {
+public class Bitwise {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        int mask = 0b0010;   //É possivel inserir a marcação de um numbero binário
+
+        if() {
+            System.out.printf("6th bit é verdadeiro");
+        } else {
+            System.out.printf("6th bit é falso");
+        }
+
+        sc.close();
     }
 }
 
